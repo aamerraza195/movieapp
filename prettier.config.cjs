99d8878e3d8b51dd3283ebd3e2e781e8205a5df7
aamerraza195 <m.aamerraza@gmail.com>
2023-03-14 17:56:36 +0500

    style: starting using eslint and prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * @type {import('prettier').Options}
+ */
+const config = {
+  semi: false,
+  singleQuote: true,
+};
+module.exports = config;
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from 'react'
-import Navbar from './compoments/Navbar';
+import React, { useState, useEffect } from 'react'
+import Navbar from './compoments/Navbar'
 import Movies from './compoments/Movies'
-import AddMovie from './compoments/AddMovie';
-import Banner from './compoments/Banner';
+import AddMovie from './compoments/AddMovie'
+import Banner from './compoments/Banner'
 import axios from './compoments/axios/axios'
 import requests from './compoments/axios/Requests'
 
@@ -10,57 +10,41 @@ function App() {
   const [movieData, setMovieData] = useState([])
   const [addMovieBtn, setAddMovieBtn] = useState(false)
 
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending)
+      setMovieData(request.data.results)
+      return request
+    }
 
+    fetchData()
+  }, [])
+  console.log(movieData)
 
-
-
-useEffect(() => {
-  async function fetchData(){
-    const request = await axios.get(requests.fetchTrending)
-    setMovieData(request.data.results)
-    return request
-}
-
-fetchData()
-}, [])
-console.log(movieData)
-
-// useEffect(()=>{
-
-//   if (movieData.length !== 0) {
-//     setMovieArray(movieData)
-//   }
-  
-// },[movieData])
-
-
-// const truncate=(string, n)=>{ return string?.length>n?string.substr(0,n-1)+'...':string
-// }
-
-
-const movieBtnHandler = ()=>{
-  setAddMovieBtn(!addMovieBtn)
-}
-const movieDataHandler = (data)=>{
-  setMovieData((prevMovieArray)=>{
-    return [{id:Math.random().toString(),...data},...prevMovieArray]
-  })
-}
-const dltMovieHandler = (movieId)=>{
-  setMovieData((prevMovieArray)=>{
-   return prevMovieArray.filter(movie=>movie.id!==movieId)       
-  })
-}
-
+  const movieBtnHandler = () => {
+    setAddMovieBtn(!addMovieBtn)
+  }
+  const movieDataHandler = (data) => {
+    setMovieData((prevMovieArray) => {
+      return [{ id: Math.random().toString(), ...data }, ...prevMovieArray]
+    })
+  }
+  const dltMovieHandler = (movieId) => {
+    setMovieData((prevMovieArray) => {
+      return prevMovieArray.filter((movie) => movie.id !== movieId)
+    })
+  }
 
   return (
     <>
-    <Navbar movieAddBtn={movieBtnHandler}/>
-    <Banner/>
-    {addMovieBtn && <AddMovie movieAddBtn={movieBtnHandler} onMovieAdd={movieDataHandler} />}
-    <Movies data={movieData} onMovieDelete={dltMovieHandler}/>
+      <Navbar movieAddBtn={movieBtnHandler} />
+      <Banner />
+      {addMovieBtn && (
+        <AddMovie movieAddBtn={movieBtnHandler} onMovieAdd={movieDataHandler} />
+      )}
+      <Movies data={movieData} onMovieDelete={dltMovieHandler} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
